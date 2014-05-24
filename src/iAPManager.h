4920ce038d32db2cec2a01261c72634922d821a8
Zoyt <nathan@flurry.name>
2014-05-24 11:01:29 -0700

    Update iAPManager.h

--- src/iAPManager.h
+++ src/iAPManager.h
@@ -1,32 +1,156 @@
 //
-//  iAPManager.h
+//  iAPManager.m
+//  PaperWars
 //
 //  Created by Caleb Jonassaint on 12/21/12.
 //  Copyright (c) 2012 theCodeMonsters. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "iAPProduct.h"
-#import "DefaultInitializer.h"
-@protocol iAPManagerDelegate <NSObject>
-@required
-- (void) errorForLoadDidOccur;
-- (void) completedTransactionForProduct:(iAPProduct *)product;
-- (void) productIsBeingPurchased:(iAPProduct *)product;
-- (void) errorOccuredForProduct:(iAPProduct *)product;
-@optional
-- (void) storeIsDoneLoading;
-- (void) restoreProduct:(iAPProduct *)product;
-- (void) productLoaded:(iAPProduct *)product;
-- (void) canceledProduct:(iAPProduct *)product;
-- (void) productsLoaded:(NSArray *)products;
+#import "iAPManager.h"
+#import <StoreKit/StoreKit.h>
+
+@interface iAPManager() <SKProductsRequestDelegate, SKPaymentTransactionObserver>
+@property (nonatomic, retain) NSMutableDictionary *products_hard;
 @end
 
-@interface iAPManager : DefaultInitializer
-@property (nonatomic, retain) NSMutableDictionary *products;
-@property (nonatomic, retain) id<iAPManagerDelegate> delegate;
-- (void) restoreProducts;
-- (void) purchaseProductForProduct:(iAPProduct *)product;
-- (BOOL)canMakePurchases;
-- (id) initWithProductIDs:(NSArray *)ids delegate:(id<iAPManagerDelegate>)delegate_T;
+@implementation iAPManager
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions
+{
+    for (SKPaymentTransaction *transaction in transactions)
+    {
+        switch (transaction.transactionState)
+        {
+            case SKPaymentTransactionStatePurchased:
+                [self completedTransaction:transaction];
+                NSLog(@"Transaction purchased");
+                break;
+            case SKPaymentTransactionStateFailed:
+                [self failedTransaction:transaction];
+                NSLog(@"Transaction f    ailed: %@",transaction.error);
+                break;
+            case SKPaymentTransactionStateRestored:
+                [self restoreTransaction:transaction];
+                NSLog(@"Transaction restored");
+                break;
+        }
+    }
+}
+
+- (void) restoreTransaction:(SKPaymentTransaction *)transaction
+{
+    iAPProduct *product = [self.products objectForKey:transaction.originalTransaction.payment.productIdentifier];
+    [self.delegate restoreProduct:product];
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+- (void) completedTransaction:(SKPaymentTransaction *)transaction
+{
+    iAPProduct *product = [self.products objectForKey:transaction.payment.productIdentifier];
+    [self.delegate completedTransactionForProduct:product];
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+- (void) failedTransaction:(SKPaymentTransaction *)transaction
+{
+    iAPProduct *product = [self.products objectForKey:transaction.payment.productIdentifier];
+    [self.delegate errorOccuredForProduct:product];
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+- (void) restoreProducts {
+    [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+}
+
+- (void) purchaseProductForProduct:(iAPProduct *)product
+{
+    SKProduct *productSK = [self.products_hard objectForKey:product.productID];
+    iAPProduct *easyProduct = [self.products objectForKey:product.productID];
+    if (product == nil) {
+        [self.delegate errorOccuredForProduct:easyProduct];
+        NSLog(@"No product");
+    } else {
+        @try {
+            [[SKPaymentQueue defaultQueue] addPayment:[SKPayment paymentWithProduct:productSK]];
+            [self.delegate productIsBeingPurchased:easyProduct];
+        }
+        @catch (NSException *exception) {
+            [self.delegate errorOccuredForProduct:easyProduct];
+            NSLog(@"Exception: %@",exception);
+        }
+    }
+}
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    NSArray *myProducts = response.products;
+    NSMutableArray *myEasyProducts = [NSMutableArray array];
+    for (unsigned int i = 0; i < myProducts.count; i++)
+    {
+        SKProduct *product = [myProducts objectAtIndex:i];
+        NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+        [numberFormatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
+        [numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+        [numberFormatter setLocale:product.priceLocale];
+        NSString *formattedString = [numberFormatter stringFromNumber:product.price];
+        
+        iAPProduct *product_easy = [iAPProduct productWithPrice:formattedString productID:product.productIdentifier title:product.localizedTitle description:product.localizedDescription];
+        [self.products_hard setObject:product forKey:product.productIdentifier];
+        [self.products setObject:product_easy forKey:product.productIdentifier];
+        if ([self.delegate respondsToSelector:@selector(productLoaded:)])
+        {
+            [self.delegate productLoaded:product_easy];
+        }
+        [myEasyProducts addObject:product_easy];
+    }
+    
+    if ([self.delegate respondsToSelector:@selector(productsLoaded:)] && myEasyProducts)
+    {
+        [self.delegate productsLoaded:myProducts];
+    }
+    else if (myEasyProducts == nil)
+    {
+        [self.delegate errorForLoadDidOccur];
+    }
+    
+    if ([self.delegate respondsToSelector:@selector(storeIsDoneLoading)])
+        [self.delegate storeIsDoneLoading];
+}
+
+- (id) initWithProductIDs:(NSArray *)ids delegate:(id<iAPManagerDelegate>)delegate_T
+{
+    if ([SKPaymentQueue canMakePayments]) {
+        // Display a store to the user.
+        self.products = [NSMutableDictionary dictionary];
+        self.products_hard = [NSMutableDictionary dictionary];
+        self.delegate = delegate_T;
+        [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+        SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithArray:ids]];
+        [request setDelegate:self];
+        [request start];
+    } else {
+        // Warn the user that purchases are disabled.
+        [self.delegate errorForLoadDidOccur];
+    }
+    return self;
+}
+
+- (void) paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+{
+    if([queue.transactions count]>0){
+        NSLog(@"Received restored transactions: %lu", (unsigned long)queue.transactions.count);
+    }
+    else {
+        NSLog(@"No Product to restore");
+        [self.delegate noProductsRestored];
+    }
+}
+
+//
+// call this before making a purchase
+//
+-(BOOL)canMakePurchases{
+    return [SKPaymentQueue canMakePayments];
+}
+
 @end
